62bb5929debd927646de2c9ec64953fa45a108f0
Ivan Kitayeu <vanya.kitaev@gmail.com>
2022-02-11 11:06:49 +0300

    Added setting to redirect student after a successful login

--- educa/settings.py
+++ educa/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.urls import reverse_lazy
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
