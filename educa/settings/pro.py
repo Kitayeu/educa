7362de82e6b10f368b141c18a2fd3b9a5b413de0
Ivan Kitayeu <vanya.kitaev@gmail.com>
2022-02-19 09:45:36 +0300

    Restricted the hosts that can serve project

--- educa/settings/pro.py
+++ educa/settings/pro.py
@@ -8,7 +8,7 @@ ADMINS = (
     ('Vanya K', os.environ.get('EMAIL_USER')),
 )
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
 
 DATABASES = {
     'default': {
